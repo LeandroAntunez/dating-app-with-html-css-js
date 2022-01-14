909b28da886ab7c0510e31f4686e85bf7d532a60
Leandro Antunez <leandro_antunez@outlook.com>
2022-01-14 18:31:55 -0300

    Click event added.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', function() {
     console.log('Page is loaded');
+
+    var searchBtn = document.getElementById('searchBtn');
+    searchBtn.addEventListener('click', function() {
+        console.log('click');
+    })
 });
